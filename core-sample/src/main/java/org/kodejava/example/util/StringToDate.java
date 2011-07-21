81093925d32cff4290c4f3a80187cb5997c09791
I Wayan Saryada <wsaryada@yahoo.com>
2011-07-21 20:29:50 +0800

    Updating the core-example.

---
+++ core-sample/src/main/java/org/kodejava/example/util/StringToDate.java
@@ -0,0 +1,19 @@
+package org.kodejava.example.util;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class StringToDate {
+    public static void main(String[] args) {
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+
+        try {
+            Date today = df.parse("20/12/2005");
+            System.out.println("Today = " + df.format(today));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
