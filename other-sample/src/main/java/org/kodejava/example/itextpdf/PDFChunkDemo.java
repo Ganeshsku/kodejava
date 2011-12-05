6483c85584b52f9c7c966300b99380f4d2d2306a
I Wayan Saryada <wsaryada@yahoo.com>
2011-12-05 22:04:38 +0800

    Update other-sample.

---
+++ other-sample/src/main/java/org/kodejava/example/itextpdf/PDFChunkDemo.java
@@ -0,0 +1,40 @@
+package org.kodejava.example.itextpdf;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.awt.*;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class PDFChunkDemo {
+    public static void main(String[] args) {
+        Document document = new Document();
+        try {
+            PdfWriter.getInstance(document,
+                    new FileOutputStream("Chunk.pdf"));
+            document.open();
+
+            //
+            // Add some chunks into the document object.
+            //
+            document.add(new Chunk("The quick brown fox "));
+            document.add(new Chunk("jumps over the lazy dog."));
+            document.add(new Chunk("The quick brown fox "));
+            document.add(new Chunk("jumps over the lazy dog."));
+            document.add(new Chunk("The quick brown fox ",
+                    FontFactory.getFont(FontFactory.HELVETICA, 20,
+                            Font.ITALIC, new BaseColor(0, 128, 0))));
+            document.add(new Chunk("jumps over the lazy dog."));
+            document.close();
+        } catch (DocumentException | FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            document.close();
+        }
+    }
+}
---
+++ other-sample/src/main/java/org/kodejava/example/itextpdf/PDFDocumentDemo.java
@@ -0,0 +1,25 @@
+package org.kodejava.example.itextpdf;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+
+public class PDFDocumentDemo {
+    public static void main(String[] args) {
+        //
+        // Creates an instance of a Document.
+        //
+        Document document = new Document();
+        try {
+            //
+            // To work with a Document we must open the document, add
+            // some contents and finally close the document.
+            //
+            document.open();
+            document.add(new Paragraph("Hello World!"));
+            document.close();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+    }
+}
