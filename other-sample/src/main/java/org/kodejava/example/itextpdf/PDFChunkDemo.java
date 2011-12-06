248d845bc6aced0e640b5cc3d88411318f868e83
unknown <wsaryada@.(none)>
2011-12-06 11:24:11 +0800

    Update other-sample.

--- other-sample/src/main/java/org/kodejava/example/itextpdf/PDFChunkDemo.java
+++ other-sample/src/main/java/org/kodejava/example/itextpdf/PDFChunkDemo.java
@@ -4,10 +4,10 @@ import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.pdf.PdfWriter;
 
-import java.awt.*;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
