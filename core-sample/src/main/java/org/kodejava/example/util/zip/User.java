59b79409ab5b41fb4b3addb9be247ba44f0474ae
wsaryada <wsaryada@yahoo.com>
2012-03-09 17:35:49 +0800

    Update core-sample examples

---
+++ core-sample/src/main/java/org/kodejava/example/util/zip/User.java
@@ -0,0 +1,69 @@
+package org.kodejava.example.util.zip;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private Long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * {@inheritDoc)
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("id=").append(id);
+        sb.append("; username=").append(username);
+        sb.append("; password=").append(password);
+        sb.append("; firstName=").append(firstName);
+        sb.append("; lastName=").append(lastName);
+
+        return sb.toString();
+    }
+}
