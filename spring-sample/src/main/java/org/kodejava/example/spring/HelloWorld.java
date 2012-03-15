ea1a72f9963575ffd76f4eb51b55d18b90ad2631
wsaryada <wsaryada@yahoo.com>
2012-03-15 10:09:39 +0800

    Update spring-sample examples

--- spring-sample/src/main/java/org/kodejava/example/spring/HelloWorld.java
+++ spring-sample/src/main/java/org/kodejava/example/spring/HelloWorld.java
@@ -1,7 +1,5 @@
 package org.kodejava.example.spring;
 
-public class HelloWorld {
-    public void sayHello() {
-        System.out.println("Hello World!");
-    }
+public interface HelloWorld {
+    void sayHello();
 }
---
+++ spring-sample/src/main/java/org/kodejava/example/spring/Piano.java
@@ -0,0 +1,9 @@
+package org.kodejava.example.spring;
+
+public class Piano implements Instrument {
+
+    @Override
+    public void play() {
+        System.out.println("Playing the Piano");
+    }
+}
