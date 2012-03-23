9a88ad9d6868154a53465f26b9b2cfc365fe454d
I Wayan Saryada <wsaryada@yahoo.com>
2012-03-23 10:03:12 +0800

    Update spring-sample, add bean scope example.

---
+++ spring-sample/src/main/java/org/kodejava/example/spring/DummyService.java
@@ -0,0 +1,13 @@
+package org.kodejava.example.spring;
+
+public class DummyService {
+    String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
